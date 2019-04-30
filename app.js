6a266b016179504ad494419656ef636cc6d4d14e
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2019-04-30 16:43:23 -0300

    configure basic query search

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const gcloud = require('./gcloudClients');
 const credentials = gcloud.getCredentials(process.env.PROJECT_ID);
 const firestore = gcloud.getFirestore(credentials);
 
+const COLLECTION_NAME = 'ownersLeads'
+
 const main = async () =>{
-  const ownersLeads = await firestore.collection('ownersLeads')
-    .where('leadUuid', '==', '00c6a27b-c231-4cde-93f0-370027444606')
+  const ownersLeads = await firestore.collection(COLLECTION_NAME)
     .get()
     .catch((err) => {
       throw new Error(err);
     });
-  ownersLeads.then(snapshot => console.log(snapshot));
+
+  ownersLeads.docs.forEach(async snapshot => {
+    const leadDoc = snapshot.data()
+    console.log(leadDoc)
+  });
 }
 
 main()
